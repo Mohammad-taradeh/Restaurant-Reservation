0a088124b4336e194ccedffff74d52d6eb01221b
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-11-28 14:08:53 +0200

    create the RestaurantReservationDbContext file

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
