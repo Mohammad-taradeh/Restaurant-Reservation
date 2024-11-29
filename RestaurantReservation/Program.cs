066e409bab49cbea43f3b6a135b7cf28dfac7932
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-11-29 23:22:52 +0200

    Console app

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Repositories;
+namespace RestaurantReservation;
+public static class Program
+{
+    private static RestaurantReservationDbContext _context = new();
+    
+    
+    
+    
+    
+    public static void Main(string[] args)
+    {
+        var empRepo = new OrderRepository(_context);
+        var result = empRepo.ListOrdersAndMenuItems(1);
+        foreach(var menuItem in result)
+        {
+            Console.WriteLine(menuItem.Order.TotalPrice);
+            Console.WriteLine(menuItem.Item.Name);
+        }
+        //empRepo.Create(new Employee() { FirstName = "123", LastName = "123", RestaurantId = 1 });
+    }
+
+}
