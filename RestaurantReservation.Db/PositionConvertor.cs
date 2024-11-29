c4643f9ae985bc9bfacbd6035f28dd0a5058968f
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-11-29 13:23:13 +0200

    Create the empolyee position converter

---
+++ RestaurantReservation.Db/PositionConvertor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RestaurantReservation.Db.Models;
+namespace RestaurantReservation.Db;
+
+[Obsolete]
+public class PositionConvertor: ValueConverter<Positions, string>
+{
+    public PositionConvertor(): base(
+        v => v.ToString(),
+        v => (Positions) Enum.Parse(typeof(Positions), v))
+    {
+        
+    }
+}
