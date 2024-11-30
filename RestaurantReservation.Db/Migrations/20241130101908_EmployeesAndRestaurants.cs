13d8c32f891b577cfcfd8b670368cddd6f85ecd2
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-11-30 12:27:30 +0200

    The migratio to create Employees and thier restaurants view

--- RestaurantReservation.Db/Migrations/20241130101908_EmployeesAndRestaurants.cs
+++ RestaurantReservation.Db/Migrations/20241130101908_EmployeesAndRestaurants.cs
@@ -10,13 +10,19 @@ namespace RestaurantReservation.Db.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"CREATE VIEW EmployeesRestaurants
+                AS
+                SELECT E.EmployeeID, E.FirstName + ' ' + E.LastName AS EmployeeName, E.Position, R.RestaurantId, R.Name As Restaurant
+                FROM Employees AS E
+                LEFT JOIN Restaurants AS R
+                ON E.RestaurantId = R.RestaurantId"
+             );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DROP VIEW EmployeesRestaurants");
         }
     }
 }
