20602e37ab3ac09762768a98b2203d140f1ec114
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-12-21 14:52:54 +0200

    remove unused using statement

--- RestaurantReservation.Db/Configurations/ReservationsCustomersAndRestaurentsConfiguration.cs
+++ RestaurantReservation.Db/Configurations/ReservationsCustomersAndRestaurentsConfiguration.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Views;
-using System.Reflection.Emit;
 namespace RestaurantReservation.Db.Configurations;
 
 public class ReservationsCustomersAndRestaurentsConfiguration : IEntityTypeConfiguration<ReservationsCustomersAndRestaurants>
 {
     public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ReservationsCustomersAndRestaurants> builder)
     {
-       builder.HasNoKey()
-           .ToView(nameof(ReservationsCustomersAndRestaurants));
+        builder.HasNoKey()
+            .ToView(nameof(ReservationsCustomersAndRestaurants));
     }
 }
